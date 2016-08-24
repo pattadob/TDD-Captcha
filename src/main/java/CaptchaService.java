fe66247a9f7e062b8f0424196ea9f6f005a0e44a
Cadet <cadet@ARCHER.local>
2016-08-24 16:56:03 +0700

    Commit after test with CaptchaService class

---
+++ src/main/java/CaptchaService.java
@@ -0,0 +1,16 @@
+
+public class CaptchaService {
+    public Randomizer randomizer;
+
+    public void setRandomizer(Randomizer randomizer) {
+        this.randomizer = randomizer;
+    }
+
+    public Captcha generateCaptcha(){
+        Captcha captcha = new Captcha(randomizer.getPattern(),
+                randomizer.getOperand(),
+                randomizer.getOperator(),
+                randomizer.getOperand());
+        return captcha;
+    }
+}
---
+++ src/test/java/CaptchaServiceUnitTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by cadet on 8/24/2016 AD.
+ */
+public class CaptchaServiceUnitTest {
+
+    @Test
+    public void shouldReturnCaptcha(){
+        CaptchaService captchaService = new CaptchaService();
+        MyRandomizer myRandomizer = new MyRandomizer();
+        captchaService.setRandomizer(myRandomizer);
+        assertEquals("ONE+1", captchaService.generateCaptcha().toString());
+        assertEquals(1, myRandomizer.callPattern);
+        assertEquals(1, myRandomizer.callOperator);
+        assertEquals(2, myRandomizer.callOperand);
+    }
+
+    public class MyRandomizer extends Randomizer{
+        public int callPattern = 0;
+        public int callOperator = 0;
+        public int callOperand = 0;
+
+        @Override
+        public int getPattern() {
+            callPattern++;
+            return 1;
+        }
+
+        @Override
+        public int getOperator() {
+            callOperator++;
+            return 1;
+        }
+
+        @Override
+        public int getOperand() {
+            callOperand++;
+            return 1;
+        }
+    }
+}
