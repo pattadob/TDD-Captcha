148667f2fc6409c3b81c0f660f5635b024e70402
Cadet <cadet@ARCHER.local>
2016-08-25 16:33:49 +0700

    Refactor ScoreAnnounce class

--- src/main/java/ScoreAnnouncer.java
+++ src/main/java/ScoreAnnouncer.java
@@ -18,42 +18,52 @@ public class ScoreAnnouncer {
         String announce = "";
 
         if (scoreA == scoreB){
-            Map<Integer, String> anounceEqualScore = new HashMap<Integer, String>();
-            anounceEqualScore.put(0, "LOVE-LOVE");
-            anounceEqualScore.put(1, "15-ALL");
-            anounceEqualScore.put(2, "30-ALL");
-            anounceEqualScore.put(3, "DEUCE");
-
-            if (scoreA < 3) {
-                announce = anounceEqualScore.get(scoreA);
-            }else {
-                announce = anounceEqualScore.get(3);
-            }
+            announce = getScoreIfBothAreEqual(scoreA);
 
         }else {
-            Map<Integer, String> tennisNumberToText = new HashMap<Integer, String>();
-            tennisNumberToText.put(0, "LOVE");
-            tennisNumberToText.put(1, "15");
-            tennisNumberToText.put(2, "30");
-            tennisNumberToText.put(3, "40");
-            tennisNumberToText.put(9999, "ADV");
-
-            if (scoreA < 3 && scoreB < 3) {
-                announce = tennisNumberToText.get(scoreA) + "-" + tennisNumberToText.get(scoreB);
-            }else {
-                if (scoreA - scoreB >= 2){
-                    announce = "A is Winner";
-                }else if (scoreB - scoreA >= 2){
-                    announce = "B is Winner";
-                }else if (scoreA - scoreB > 1){
-                    announce = tennisNumberToText.get(9999) + "-A" ;
-                }else {
-                    announce = tennisNumberToText.get(9999) + "-B" ;
-                }
-
-            }
+            announce = getScoreIfNotEqual(scoreA, scoreB);
         }
 
+        return announce;
+    }
+
+    private String getScoreIfNotEqual(int scoreA, int scoreB) {
+        String announce;Map<Integer, String> tennisNumberToText = new HashMap<Integer, String>();
+        tennisNumberToText.put(0, "LOVE");
+        tennisNumberToText.put(1, "15");
+        tennisNumberToText.put(2, "30");
+        tennisNumberToText.put(3, "40");
+        tennisNumberToText.put(9999, "ADV");
+
+        if (scoreA < 3 && scoreB < 3) {
+            announce = tennisNumberToText.get(scoreA) + "-" + tennisNumberToText.get(scoreB);
+        }else {
+            if (scoreA - scoreB >= 2){
+                announce = "A is Winner";
+            }else if (scoreB - scoreA >= 2){
+                announce = "B is Winner";
+            }else if (scoreA - scoreB > 1){
+                announce = tennisNumberToText.get(9999) + "-A" ;
+            }else {
+                announce = tennisNumberToText.get(9999) + "-B" ;
+            }
+
+        }
+        return announce;
+    }
+
+    private String getScoreIfBothAreEqual(int scoreA) {
+        String announce;Map<Integer, String> anounceEqualScore = new HashMap<Integer, String>();
+        anounceEqualScore.put(0, "LOVE-LOVE");
+        anounceEqualScore.put(1, "15-ALL");
+        anounceEqualScore.put(2, "30-ALL");
+        anounceEqualScore.put(3, "DEUCE");
+
+        if (scoreA < 3) {
+            announce = anounceEqualScore.get(scoreA);
+        }else {
+            announce = anounceEqualScore.get(3);
+        }
         return announce;
     }
 }
